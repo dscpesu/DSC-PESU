dace08dfc0e7237becaf7d756449ed7b96838c2b
Varun Gupta <varungupta3009@gmail.com>
2019-05-14 18:25:14 +0530

    Updates

---
+++ precache-manifest.bac145d544fb4b6b7dc903ba2816bbaa.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = [
+  {
+    "revision": "a550c94977ee5a232089",
+    "url": "/DSC-PESU/static/css/main.61d05f9c.chunk.css"
+  },
+  {
+    "revision": "a550c94977ee5a232089",
+    "url": "/DSC-PESU/static/js/main.397b7ef8.chunk.js"
+  },
+  {
+    "revision": "bc951fc5e7dbf964e0dc",
+    "url": "/DSC-PESU/static/js/runtime~main.5dec7e2f.js"
+  },
+  {
+    "revision": "d5d411192da356fe7df3",
+    "url": "/DSC-PESU/static/css/2.01c7df8d.chunk.css"
+  },
+  {
+    "revision": "d5d411192da356fe7df3",
+    "url": "/DSC-PESU/static/js/2.683a7cd7.chunk.js"
+  },
+  {
+    "revision": "991411505951f7763636c7f0fa823803",
+    "url": "/DSC-PESU/static/media/LinkedIn.99141150.svg"
+  },
+  {
+    "revision": "a861f00352e24640d84add0f3d9393cb",
+    "url": "/DSC-PESU/static/media/GitHub.a861f003.svg"
+  },
+  {
+    "revision": "f4fd696f8e4302f4c4a8e5cbbae9e2d8",
+    "url": "/DSC-PESU/static/media/Facebook.f4fd696f.svg"
+  },
+  {
+    "revision": "f1756ea2cd91f823fa9a55b3e03b6506",
+    "url": "/DSC-PESU/static/media/Twitter.f1756ea2.svg"
+  },
+  {
+    "revision": "901657aed4830ebbf866910d6da73002",
+    "url": "/DSC-PESU/static/media/Instagram.901657ae.svg"
+  },
+  {
+    "revision": "f5679741b14945b987e6a235b0c993e3",
+    "url": "/DSC-PESU/static/media/GMail.f5679741.svg"
+  },
+  {
+    "revision": "c2c5f62ddbd03435e88c3792b9ff71b7",
+    "url": "/DSC-PESU/static/media/DSC-H.c2c5f62d.png"
+  },
+  {
+    "revision": "300f3bec7bdaf7001c9f28184235b88b",
+    "url": "/DSC-PESU/static/media/DSC-Badge-by-VRG.300f3bec.svg"
+  },
+  {
+    "revision": "318f534d72229d03d8ffc5f7a34b84b1",
+    "url": "/DSC-PESU/static/media/Lord-Commander.318f534d.jpg"
+  },
+  {
+    "revision": "3df4db3c780dda8cb868d7d3d9533309",
+    "url": "/DSC-PESU/static/media/Yash.3df4db3c.jpg"
+  },
+  {
+    "revision": "cbab995b916ef4e4b0d5df13feb3bd48",
+    "url": "/DSC-PESU/static/media/Anudeep.cbab995b.jpg"
+  },
+  {
+    "revision": "b0176c87d6c520f33ca0be90e5863ad0",
+    "url": "/DSC-PESU/static/media/Mayank.b0176c87.jpeg"
+  },
+  {
+    "revision": "23f80a3f05151d258dd22a07b0157336",
+    "url": "/DSC-PESU/static/media/Shivangi.23f80a3f.jpg"
+  },
+  {
+    "revision": "87fb78e2d1c2cc7f4e62473a68dc125e",
+    "url": "/DSC-PESU/static/media/Sumanth.87fb78e2.jpg"
+  },
+  {
+    "revision": "d4fe5116e241148b1f17ab00f7684c4b",
+    "url": "/DSC-PESU/static/media/VRG [AF].d4fe5116.png"
+  },
+  {
+    "revision": "d29264daa38fd90e77015a7260f86539",
+    "url": "/DSC-PESU/static/media/Shankar.d29264da.jpg"
+  },
+  {
+    "revision": "9be250f1aff4146f2f36d2461da7ada8",
+    "url": "/DSC-PESU/static/media/Ishaan.9be250f1.jpg"
+  },
+  {
+    "revision": "8ed0ab3749572884629e4527e9579c9d",
+    "url": "/DSC-PESU/static/media/Richa.8ed0ab37.jpg"
+  },
+  {
+    "revision": "92a4fcf5422dd04d9fb82689cf4a3b17",
+    "url": "/DSC-PESU/static/media/Akash.92a4fcf5.jpg"
+  },
+  {
+    "revision": "1ac53243cad31718855cbc8ed0ddd16e",
+    "url": "/DSC-PESU/static/media/Ishwar.1ac53243.jpeg"
+  },
+  {
+    "revision": "dc66e7b9659cd81232f9f3b1d9a937e1",
+    "url": "/DSC-PESU/static/media/DSC-V.dc66e7b9.png"
+  },
+  {
+    "revision": "00955c347a8db0e81ee8c3695b0ce073",
+    "url": "/DSC-PESU/static/media/Google Dots by VRG.00955c34.gif"
+  },
+  {
+    "revision": "682656795a0e4f7e351d81491ae48bfd",
+    "url": "/DSC-PESU/index.html"
+  }
+];
